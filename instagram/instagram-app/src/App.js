f89977e950179e4644c1450401d3aca12a5cc96d
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-05-22 21:17:25 -0400

    started building out search bar and importing it to App.js file

--- instagram/instagram-app/src/App.js
+++ instagram/instagram-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div>Instagram Clone</div>
+    <div className="App">
+      <SearchBar />
+    </div>
   );
 }
 
